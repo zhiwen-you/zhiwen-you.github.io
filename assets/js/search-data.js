bcd917e586d006038eb005308cbf314f6a7dfa37
zhiwen-you <youzhiwen_2019@163.com>
2025-03-08 23:02:22 +0000

    Deploying to gh-pages from @ zhiwen-you/zhiwen-you.github.io@57e50407ab46e537faf1b5889ef22ade9825146e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,7 +429,10 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-party-popper-party-popper-party-popper-beyond-binary-gender-labels-is-accepted-by-the-5th-workshop-on-gender-bias-in-natural-language-processing-co-located-at-acl-2024",
+            },},{id: "news-sparkles-we-won-the-first-place-trophy-at-biolaysumm-competition-over-53-teams",
+          title: ':sparkles: We won the first place :trophy: at BioLaySumm competition over 53 teams!...',
+          description: "",
+          section: "News",},{id: "news-party-popper-party-popper-party-popper-beyond-binary-gender-labels-is-accepted-by-the-5th-workshop-on-gender-bias-in-natural-language-processing-co-located-at-acl-2024",
           title: ':party_popper::party_popper::party_popper:Beyond Binary Gender Labels is accepted by the 5th Workshop on Gender Bias...',
           description: "",
           section: "News",},{id: "news-received-researcher-access-award-from-openai-sparkles-smile",
