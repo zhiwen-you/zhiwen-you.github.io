6cdf0b92c4e18c6b41d3543022abdad32c83fdf4
zhiwen-you <youzhiwen_2019@163.com>
2025-03-09 03:19:28 +0000

    Deploying to gh-pages from @ zhiwen-you/zhiwen-you.github.io@170e92e047ba6037b644fa020c22820cddf324c4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-teaching-assistant",
-          title: "Teaching Assistant",
+        },{id: "nav-teaching",
+          title: "Teaching",
           description: "",
           section: "Navigation",
           handler: () => {
